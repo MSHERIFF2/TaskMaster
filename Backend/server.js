b2b8446382b0a07586f296f7647dc7ec01a4f03a
Sheriffdeen Muritala Adegoke <muritalasheriffdeenadegoke@gmail.com>
2024-11-25 13:56:22 +0000

    nodemon

--- Backend/server.js
+++ Backend/server.js
@@ -17,12 +17,20 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
+// testing API
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Taskmaser API')
+})
 // MongoDB connection
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected successfully"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
+// JWT SECRET TOKEN
+const jwtScrete = process.env.JWT_SECRET;
+
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5100;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
