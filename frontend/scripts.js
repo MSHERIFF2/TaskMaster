d8657a10396d2bdcea815864da6693d1dbfdbac7
Sheriffdeen Muritala Adegoke <muritalasheriffdeenadegoke@gmail.com>
2024-11-26 08:41:00 +0000

    updated

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,5 +1,5 @@
 // API base URL for backend
-const API_URL = 'http://localhost:5000/api';  // Update if necessary
+const API_URL = 'https://taskmaster-lwpe.onrender.com/';  // Update if necessary
 const taskList = document.querySelector(".task-list");
 
 // Register Page
